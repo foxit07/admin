4a7802b4a80a58c0c8f6dd6a6ee4cec822bbb074
foxit07 <rotokan2@gmail.com>
2019-12-27 08:25:55 +0000

    Minify styles

--- resources/publishable/assets/js/admin.js
+++ resources/publishable/assets/js/admin.js
@@ -1,14 +1,2 @@
-(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["/js/admin"],[
-/* 0 */,
-/* 1 */
-/*!***********************************************************************!*\
-  !*** multi ./node_modules/startbootstrap-sb-admin-2/js/sb-admin-2.js ***!
-  \***********************************************************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-module.exports = __webpack_require__(/*! /var/www/package_admin/node_modules/startbootstrap-sb-admin-2/js/sb-admin-2.js */"./node_modules/startbootstrap-sb-admin-2/js/sb-admin-2.js");
-
-
-/***/ })
-],[[1,"/js/manifest","/js/vendor"]]]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],[,function(w,n,o){w.exports=o("w+b4")}],[[1,0,1]]]);
+//# sourceMappingURL=admin.js.map
